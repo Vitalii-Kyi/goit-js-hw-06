50cef8886b276402b93d2593ab6129ba58c570c2
Vitalii Kyi <kyi.profy@gmail.com>
2023-06-05 21:24:55 +0300

    task 02, 03

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ul = document.querySelector('#ingredients');
+
+const liEl = ingredients.map(ingredients => {
+  const li = document.createElement('li');
+
+  li.textContent = ingredients;
+  li.classList.add('item');
+
+  return li;
+});
+
+ul.append(...liEl);
--- src/js/task-03.js
+++ src/js/task-03.js
@@ -1,3 +1,5 @@
+const galleryList = document.querySelector('.gallery');
+
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -12,3 +14,13 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const imagesMarkup = images.map(({ url, alt }) => {
+ return `
+  <li class="gallery__item">
+    <img class="gallery__image" src="${url}" alt="${alt}">
+  </li >
+  `;
+}).join('');
+
+galleryList.insertAdjacentHTML('beforeend', imagesMarkup);
